a662d13b43d8567e51e849c2c2e93ee1a6db0f0f
Bima-010 <adikabima010@gmail.com>
2025-12-01 15:37:52 +0700

    Delete index.php, dipindah ke admin mejadi index-admin.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include 'includes/header.php'; ?>
-
-<h2>Selamat Datang di Dashboard Admin</h2>
-<p>Silakan pilih menu di atas untuk mengelola konten website.</p>
-
-<?php include 'includes/footer.php'; ?>
